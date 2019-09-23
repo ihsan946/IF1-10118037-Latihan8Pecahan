6de3b0884bb990270a1a4c37edf6e8ff19c04f56
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-09-23 14:53:43 +0700

    Add files via upload

---
+++ IF110118037Latihan8Pecahan/src/if110118037latihan8pecahan/IF110118037Latihan8Pecahan.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if110118037latihan8pecahan;
+
+/**
+ * @author LENOVO
+ * NAMA      : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM       : 10118037
+ * Deskripsi Program   : Program ini berisi program dengan variabel
+ *                       Pecahan.
+ */
+public class IF110118037Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float : " + f);
+        System.out.println("Double : " + d);
+        
+               
+    }
+    
+}
